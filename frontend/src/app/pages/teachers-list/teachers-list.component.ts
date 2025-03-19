5f78df5d9162f5c3fd98f28a2a14ee066d1f4087
Ntando Magana <ntandomagana@gmail.com>
2025-03-19 21:50:37 +0200

    fix the routing issue

---
+++ frontend/src/app/pages/teachers-list/teachers-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-teachers-list',
+  imports: [RouterModule],
+  templateUrl: './teachers-list.component.html',
+  styleUrl: './teachers-list.component.css'
+})
+export class TeachersListComponent {
+
+}
