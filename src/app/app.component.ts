6bb223f0cbb40ff6b0eb7f037dc940e56d378991
ntandomagana <ntandomagana@gmail.com>
2025-03-19 15:39:01 +0200

    add mor epictures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { LandingPageComponent } from '../components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent,
+    LandingPageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
