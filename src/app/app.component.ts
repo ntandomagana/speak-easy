988a7fa6fc77f55eb630288015bf496458681662
ntandomagana <ntandomagana@gmail.com>
2025-03-20 09:04:57 +0200

    workon the teachers list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../components/navbar/navbar.component';
-import { LandingPageComponent } from '../components/landing-page/landing-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
