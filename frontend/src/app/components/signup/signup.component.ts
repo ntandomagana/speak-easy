233d3de8e5fc288cf597a9303a5b79155e71c13e
ntandomagana <ntandomagana@gmail.com>
2025-04-02 16:08:18 +0200

    signup page doesn't have validators

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  isLoading = false;
+
+
+
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.signupForm =  this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      
+
+    });
+  
+  }
+
+  ngOnInit(): void {
+  }
+   
+  onSubmit(){
+    if (this.signupForm.valid) {
+      alert('Yoou successfully signed up!')
+    }
+  }
+
 
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    darkMode: 'class', // Can also use 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
