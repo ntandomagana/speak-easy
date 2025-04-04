01fd827e5fafa92e96f2e8872646e059d43d1878
ntandomagana <ntandomagana@gmail.com>
2025-04-04 10:31:52 +0200

    added validation errors

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -2,43 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-signup',
-  imports: [ReactiveFormsModule,
-    CommonModule,
-    RouterLink
-  ],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
   isLoading = false;
-
-
-
   signupForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-
-    this.signupForm =  this.fb.group({
+    this.signupForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      
-
     });
-  
   }
 
-  ngOnInit(): void {
-  }
-   
-  onSubmit(){
+  ngOnInit(): void {}
+
+  onSubmit() {
     if (this.signupForm.valid) {
-      alert('Yoou successfully signed up!')
+      alert('You successfully signed up!');
+      console.log(this.signupForm.value);
+    } else {
+      console.log('Form is invalid');
     }
   }
 
-
+  
 }
