ed78cf7d8f92dd4c22265318670ced6dbb2a337e
Ntando Magana <ntandomagana@gmail.com>
2025-04-13 02:18:40 +0200

    backend changes, the signup does not work

--- backend/src/main/java/backend/services/AppUserService.java
+++ backend/src/main/java/backend/services/AppUserService.java
@@ -1,9 +1,11 @@
 package backend.services;
 
+import backend.dtos.SignupDTO;
 import backend.models.AppUser;
 
 public interface AppUserService {
-
+;
+    AppUser registeruser(SignupDTO appUserDTO)
     AppUser saveUser(AppUser user);
     AppUser findByEmail(String email);
 }
--- backend/src/main/java/backend/services/AppUserServiceImpl.java
+++ backend/src/main/java/backend/services/AppUserServiceImpl.java
@@ -1,5 +1,32 @@
+import backend.dtos.SignupDTO;
+import backend.models.AppUser;
+import backend.repositories.AppUserRepository;
+import backend.services.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
-public class AppUserServiceImpl {
+public class AppUserServiceImpl implements AppUserService {
+
+private final AppUserRepository appUserRepository;
+
+@Autowired
+    public AppUserServiceImpl(AppUserRepository appUserRepository) {
+    this.appUserRepository = appUserRepository;
+}
+
+@Override
+    public AppUser registerUser(SignupDTO dto) {
+    AppUser user = new AppUser();
+
+    user.setName(dto.getName());
+    user.setEmail(dto.getEmail());
+    user.setPassword(dto.getPassword());
+    user.setRole(dto.getRole());
+
+    return appUserRepository.save(user);
+
+}
 
 
 }
