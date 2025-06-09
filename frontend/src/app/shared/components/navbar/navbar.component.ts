2429a3edc5c33d27655b7013fbd88c40949babde
Ntando Magana <ntandomagana@gmail.com>
2025-06-09 21:44:49 +0200

    navabr changes when user logs in, but not when they sign up

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
-   isLoggedIn = false;
-  userRole: string | null = null;
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('loggedInUserInfo');
+  }
 
-  constructor(private authService: AuthService, private router: Router) {}
-
-  ngOnInit() {
-    this.isLoggedIn = this.authService.isLoggedIn();
-    this.userRole = this.authService.getUserRole();
+  getUserRole(): string | null {
+    const user = localStorage.getItem('loggedInUserInfo');
+    if (user) {
+      return JSON.parse(user).role || null;
+    }
+    return null;
   }
 
   logout() {
     localStorage.clear();
     this.router.navigate(['/login']);
-    this.isLoggedIn = false;
-    this.userRole = null;
-
-}
+  }
 }
